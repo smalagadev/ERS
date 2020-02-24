578be3d2a3eadcd7ac2e9e3fefd02d8668e00723
Steffano <smalagadev@gmail.com>
2020-02-24 15:18:26 -0500

    models & services initialized

---
+++ client/src/app/components/models/user.ts
@@ -0,0 +1,9 @@
+export class User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  username: string;
+  password: string;
+  email: string;
+  role_id: number;
+}
---
+++ client/src/app/components/services/reimbursement.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Reimbursement } from '../models/reimbursement';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReimbursementService {
+
+  constructor(private http: HttpClient) { }
+
+  // insert(amount:number, description:string, receipt:object, type_id:number, user_id:number ):Observable<Reimbursement>{
+  //   let body:any = {
+  //     amount: amount,
+  //     description: description,
+  //     receipt: receipt,
+  //     type_id: type_id,
+  //     author: user_id
+  //   }
+  //
+  //   return this.http.post<Reimbursement>('http://localhost:8080/project-1/new_reimbursements', body);
+  // }
+  //
+  // userViewAll(user_id: number):Observable<Reimbursement>{
+  //   let body:any = {
+  //     user_id: user_id
+  //   }
+  //
+  //   return this.http.post<Reimbursement>('http://localhost:8080/project-1/reimbursements', body);
+  // }
+  //
+  // managerViewAll(): Observable<Reimbursement>{
+  //   let body:any = {}
+  //
+  //   return this.http.get<Reimbursement>('http://localhost:8080/project-1/reimbursements', body);
+  // }
+
+  // statusUpdate(reimbursement_id: number, statusChange: number, resolver_id: number) :Observable<Reimbursement>{
+  //   let body ={
+  //     resolver: resolver_id,
+  //     reimbursement_id: reimbursement.id,
+  //     status_id: statusChange
+  //   }
+  //
+  //   return this.http.post<Reimbursement>('http://localhost:8080/project-1/update_reimbursements', body);
+  // }
+}
